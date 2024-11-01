a3946c7043cf9ad512002af3581065958330a54b
luismiguelmartinluengo <luismiguelm@gmail.com>
2024-11-01 11:18:23 +0000

    Diseño y test de clase FieldsSelector

---
+++ src/main/java/com/lmml/datafiles/Explorer/FieldsSelector.java
@@ -0,0 +1,38 @@
+package com.lmml.datafiles.Explorer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FieldsSelector {
+    /*Este objeto asume que el array que representa la linea de la que tiene que extraer
+     * los valores siempre es correcta, es decir, que encaja con el Descriptor de fichero
+     * que se ha usado para construir el selector de campos. Por lo tanto la longitud del array
+     * pasado a getSelección siempre será igual o superior al índice máximo almacenado en el 
+     * array indices
+     */
+   
+    private int[] indices = new int[0];
+    
+    public String[] getSelection(String[] _lineList){
+        List<String> extracted = IntStream.of(indices).mapToObj(i -> _lineList[i]).collect(Collectors.toList());
+        return extracted.toArray(new String[extracted.size()]);
+    }//End for
+
+    FieldsSelector(FileDescriptor _fileDescriptor, String[] _fields){
+        TreeSet<Integer> fieldsIndexSet = new TreeSet<Integer>();
+        HashSet<String> fieldsSet = new HashSet<String>(Arrays.asList(_fields));
+        ArrayList<String> fileDescriptorFields = new ArrayList<String>(Arrays.asList(_fileDescriptor.getHeads()));
+        for(String field : fieldsSet){
+            fieldsIndexSet.add(fileDescriptorFields.indexOf(field));
+        }//End for
+        fieldsIndexSet.remove(-1);//quita el valor de los campos no encontrados si existiese
+        indices = fieldsIndexSet.stream().mapToInt(Integer::intValue).toArray();
+    }//End Constructor
+
+}//End FieldsSelector
---
+++ src/test/java/com/lmml/datafiles/Explorer/FieldsSelectorTest.java
@@ -0,0 +1,82 @@
+package com.lmml.datafiles.Explorer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class FieldsSelectorTest {
+    /*Como el objeto presupone siempre que la lista de valores va a encarjar con la longitud de 
+     * encabezados especificados por el descriptor de fichero, no proceden pruebas para los casos
+     * de que la lista de valores sea superior o inferior a la esperada.
+    */
+
+    String[] descriptorHeads = {"Col1","Col2","Col3","Col4","Col5"};
+    private FileDescriptor fileDescriptor = new FileDescriptor("", ',', '\"', descriptorHeads, 1);
+    String[] testValues = {"1","2","3","4","5"};
+
+    @Test
+    public void testConstructorFullLine(){
+        String[] testSelection = descriptorHeads ;
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = testValues;
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorPartialSelection(){
+        String[] testSelection ={"Col1","Col3","Col5"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","3","5"};;
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorPartialSelectionWithRepeatedHeads(){
+        String[] testSelection ={"Col1","Col1","Col3","Col3","Col3"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","3"};;
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorExceededSelection(){
+        String[] testSelection ={"Col1","Col2","Col6","Col3","Col4","Col5","Col7"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","2","3","4","5"};
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorExceededSelectionWithRepeatedHeads(){
+        String[] testSelection ={"Col1","Col2","Col7","Col4","Col4","Col5","Col7"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","2","4","5"};
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorAssortedSelection(){
+        String[] testSelection ={"Col3","Col1","Col5","Col2","Col4"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","2","3","4","5"};
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+    @Test
+    public void testConstructorPartialAssortedExceededSelectionWithRepeatedHeads(){
+        String[] testSelection ={"Col3","Col1","Col1","Col4","Col6","Col7","Col7","Col2","Col4"}; 
+        FieldsSelector fieldsSelector = new FieldsSelector(fileDescriptor, testSelection);
+        String[] expectedResult = {"1","2","3","4"};
+        String[] result = fieldsSelector.getSelection(testValues);
+        assertTrue(Arrays.equals(expectedResult, result));
+    }//End test
+
+}//End FieldsSelectorTest
